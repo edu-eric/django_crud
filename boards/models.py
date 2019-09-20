2063120b50c2c28602fb65eb370bef00f3d777f9
이민교 <eric@imingyoui-MacBookPro.local>
2019-09-20 13:37:46 +0900

    eric | Post 모델 작성 완료

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
