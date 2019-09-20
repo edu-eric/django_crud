7d627829117f23500125cc8c6f8474a3aac9a7d0
이민교 <eric@imingyoui-MacBookPro.local>
2019-09-20 13:03:02 +0900

    eric | 장고 프로젝트 생성완료

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/eric/HPHK/HPHK/강의자료 by Me/[SERIES] 강의 코드 with git - 실전/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
