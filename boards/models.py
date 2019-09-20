5b3a0c13e63418020d3d9b6776469d328795a954
이민교 <eric@imingyoui-MacBookPro.local>
2019-09-20 13:39:54 +0900

    eric | all() 메서드를 사용해서 해당 모델의 레코드 전부 가져오기

--- boards/models.py
+++ boards/models.py
@@ -5,4 +5,5 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True
+    
