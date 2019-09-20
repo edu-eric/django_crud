dd86931ea74dff9aba1e936bc75948ba13fd3eb7
이민교 <eric@imingyoui-MacBookPro.local>
2019-09-20 22:34:24 +0900

    eric | 댓글 기능을 위한 Comment 모델 작성

--- boards/models.py
+++ boards/models.py
@@ -6,3 +6,8 @@ class Post(models.Model):
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+class Comment(models.Model):
+    name = models.CharField(max_length=10)
+    content = models.CharField(max_length=100)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
